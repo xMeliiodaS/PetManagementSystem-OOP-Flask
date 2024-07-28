88430087449c94a03dde746b3f819256424e668a
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-07-28 19:40:04 +0300

    Modified

--- PetManagementFlaskOOP/pythonProject/app.py
+++ PetManagementFlaskOOP/pythonProject/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-from load_json import load_data, save_data
 from owner_management import OwnerManagement
 
 app = Flask(__name__)
@@ -16,6 +14,22 @@ def home_page():
 def owner_page():
     listed_owners = owner_management.list_owners()
     return render_template('owner_page.html', owners=listed_owners)
+
+
+@app.route('/add_owner', methods=['GET', 'POST'])
+def add_owner():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        new_owner = {
+            'owner_name': name,
+            'phone_number': phone,
+            'pets': []
+        }
+        owner_management.add_owner(new_owner)
+        return redirect(url_for('owner_page'))
+
+    return render_template('add_owner.html')
 
 
 @app.route('/add_pet/<owner_phone_number>')
