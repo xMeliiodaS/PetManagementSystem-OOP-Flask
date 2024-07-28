c8a965a2d5707d524681e9f065ff0ccc107b4d89
NOTxMeliiodaaS <kotarou.gaia557@gmail.com>
2024-07-28 14:16:51 +0300

    Created 2 html pages and flask app

---
+++ PetManagementFlaskOOP/pythonProject/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+# from load_json import load_data, save_data
+
+app = Flask(__name__)
+# data = load_data()
+
+
+@app.route('/')
+def home_page():
+    return render_template('home_page.html')
+
+@app.route('/owner')
+def owner_page():
+    return render_template('owner_page.html')
