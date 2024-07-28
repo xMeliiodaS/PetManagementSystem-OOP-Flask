9ccd25057fc84f0545cede568a13bae1ae1bbb1a
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-07-28 18:00:24 +0300

    Added the owner manager script also added flask in the html

--- PetManagementFlaskOOP/pythonProject/app.py
+++ PetManagementFlaskOOP/pythonProject/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 from load_json import load_data, save_data
+from owner_management import OwnerManagement
 
 app = Flask(__name__)
-data = load_data()
+owner_management = OwnerManagement()
 
 
 @app.route('/')
@@ -13,4 +14,21 @@ def home_page():
 
 @app.route('/owner')
 def owner_page():
-    return render_template('owner_page.html')
+    listed_owners = owner_management.list_owners()
+    return render_template('owner_page.html', owners=listed_owners)
+
+
+@app.route('/add_pet/<owner_phone_number>')
+def add_pet(owner_phone_number):
+    # Implement the logic to add a pet to the owner
+    return f"Add pet for owner with phone number {owner_phone_number}"
+
+
+@app.route('/pets_list/<owner_phone_number>')
+def pets_list(owner_phone_number):
+    # Implement the logic to list pets for the owner
+    return f"List of pets for owner with phone number {owner_phone_number}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ PetManagementFlaskOOP/pythonProject/owner_management.py
@@ -0,0 +1,59 @@
+import json
+from pet_management.owner import Owner
+
+
+class OwnerManagement:
+    def __init__(self, filename="pet_database.json"):
+        """
+        Initialize a Library object.
+
+        Args:
+        - filename (str, optional): File name to save/load library data (default is 'library.json').
+        """
+        self.filename = filename
+        self.owners = []
+        self.load_owners()
+
+    def add_owner(self, owner):
+        """
+        Add a book to the library and save library data to file.
+
+        Args:
+        - book (Book): Book object to add to the library.
+        """
+        self.owners.append(owner)
+        self.save_owners()
+
+    def add_pet_to_owner(self, pet, owner_phone_number):
+        for owner in self.owners:
+            if owner.phone_number == owner_phone_number:
+                owner.add_pet(pet)
+                break
+
+    def remove_pet_from_owner(self, pet, owner_phone_number):
+        for owner in self.owners:
+            if owner.phone_number == owner_phone_number:
+                owner.remove_pet(pet)
+                break
+
+    def list_owners(self):
+        return self.owners
+
+    def save_owners(self):
+        """
+        Save the current state of the library (books) to a JSON file.
+        """
+        with open(self.filename, 'w') as file:
+            json.dump([owner.to_dict() for owner in self.owners], file)
+
+    def load_owners(self):
+        """
+        Load library data from a JSON file into the library (books).
+        If the file does not exist, initialize an empty library.
+        """
+        try:
+            with open(self.filename, 'r') as file:
+                owner_dicts = json.load(file)
+                self.owners = [Owner(**owner_dict) for owner_dict in owner_dicts]
+        except FileNotFoundError:
+            self.owners = []
